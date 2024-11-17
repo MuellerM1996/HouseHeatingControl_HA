313bf39a5cac99b8d2c5854dc2252af094324e51
Markus Müller <mueller.markus1996@outlook.de>
2024-11-17 22:27:08 +0100

    bugfix

--- custom_components/home_heat_control/const.py
+++ custom_components/home_heat_control/const.py
@@ -16,14 +16,14 @@ ATTR_STATUS_DESCRIPTION = "status_description"
 ATTR_MANUFACTURER = "MM/HL Engineering"
 CONF_MODBUS_ADDRESS = "modbus_address"
 
-#attributes: type(0=normal | 1=binary), name, key, unit, class, icon
+#attributes: type(0=normal | 1=binary), name, key is mandatory
 HHCSENSOR_TYPES = [
     [0, SensorEntityDescription(name="FBL Software Version", key="fbl_sw_version")],
     [0, SensorEntityDescription(name="APPL Software Version", key="appl_sw_version")],
-    [1, SensorEntityDescription(name="DTCs Aktiv", key="dtcactive", device_class=BinarySensorDeviceClass.PROBLEM)],
+    [1, BinarySensorEntityDescription(name="DTCs Aktiv", key="dtcactive", device_class=BinarySensorDeviceClass.PROBLEM)],
     [0, SensorEntityDescription(name="Außentemperatur", key="outsidetemperature", state_class=SensorStateClass.MEASUREMENT, device_class=SensorDeviceClass.TEMPERATURE, unit_of_measurement=UnitOfTemperature.CELSIUS)],
     [0, SensorEntityDescription(name="Raum 1 Temperatur", key="room1temperature", state_class=SensorStateClass.MEASUREMENT, device_class=SensorDeviceClass.TEMPERATURE, unit_of_measurement=UnitOfTemperature.CELSIUS)],
     [0, SensorEntityDescription(name="Raum 2 Temperatur", key="room2temperature", state_class=SensorStateClass.MEASUREMENT, device_class=SensorDeviceClass.TEMPERATURE, unit_of_measurement=UnitOfTemperature.CELSIUS)],
     [0, SensorEntityDescription(name="Türklingel Status" , key="doorbell_status", device_class=SensorDeviceClass.ENUM, icon="mdi:bell")],
-    [1, SensorEntityDescription(name="Hauptschalter", key="heatcontrolmanagement_enabled")],
+    [1, BinarySensorEntityDescription(name="Hauptschalter", key="heatcontrolmanagement_enabled")],
 ]
